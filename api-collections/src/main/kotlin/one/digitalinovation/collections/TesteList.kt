42f88971d7b5908ecceff393da8faae2aaa49911
euridesl <eurides.lisboa@gmail.com>
2021-03-11 21:09:34 -0300

    operacao com .ordbay, sortby

---
+++ api-collections/src/main/kotlin/one/digitalinovation/collections/TesteList.kt
@@ -0,0 +1,40 @@
+package one.digitalinovation.collections
+
+fun main() {
+    val joao = Funcionario("joao", 2000.0 ,"clt")
+    val pedro = Funcionario("pedro", 3000.0,"PJ")
+    val maria = Funcionario("maria", 1000.0,"PJ")
+
+ val funcionarios = listOf(joao,pedro,maria)
+
+    funcionarios.forEach{ println(it)}
+    // buscando somente maria
+println("---------------------------------------------")
+    println(funcionarios.find {it.nome == "maria"})
+println("------------------------------------")
+    //ordenando com sortby e terminando comforech para impressao
+
+    funcionarios
+
+        .groupBy { it.TipoContratacao }
+        .forEach { println(it) }
+
+
+    }
+
+
+data class Funcionario(
+    val nome : String,
+    val salario : Double,
+    val TipoContratacao :String
+
+){
+
+    //subscrever o toString
+
+    override fun toString(): String ="""
+        nome    : $nome
+        Salario : $salario
+    """.trimIndent()
+
+}
